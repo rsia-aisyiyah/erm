0589581d4a6d758e97071f57112216dad7d2cb64
msfiqih <msfiqih@gmail.com>
2023-02-25 13:30:37 +0700

    modul askep kebidanan

---
+++ resources/views/content/poliklinik/modal/modal_askep.blade.php
@@ -0,0 +1,24 @@
+<div class="modal fade" id="modalAsmed" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-fullscreen">
+        <div class="modal-content" style="background-color: #e7e7e7;">
+            {{-- <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">PEMERIKSAAN S.O.A.P</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div> --}}
+            <div class="modal-body">
+                <h6 class="text-center">ASESMEN PENILAIAN PASIEN RAWAT JALAN KEBIDANAN & KANDUNGAN</h6>
+                <table class="borderless">
+
+                </table>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger btn-sm" data-bs-dismiss="modal"><i
+                        class="bi bi-x-circle"></i> Keluar</button>
+
+            </div>
+        </div>
+    </div>
+</div>
+@push('script')
+    <script></script>
+@endpush
