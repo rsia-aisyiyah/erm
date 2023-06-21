0afeec5ce08a84e91dce83bf2cbc645df3e2e4c4
FAISAL HALIM <ffaisalhalim@gmail.com>
2023-06-21 14:34:19 +0700

    update

--- resources/views/antrian.blade.php
+++ resources/views/antrian.blade.php
@@ -13,24 +13,24 @@
 </head>
 
 <body>
-    <div class="w-full h-[100vh] bg-green-100 flex items-center align-items-center">
-        <div class="w-[95%] m-auto flex flex-col gap-0">
-            <div class="p-3 border-2 border-b-0 border-green-600 border-gray-400">
-                <p class="font-bold text-green-700 text-5xl text-center">
-                    LOKET ANTRIAN
+    <div class="w-full h-[100vh] flex items-center align-items-center">
+        <div class="w-[95%] h-[90%] m-auto flex flex-col gap-0">
+            <div class="p-5 border-2 xl:border-4 xl:border-b-0 border-b-0 border-blue-500">
+                <p class="font-bold text-blue-500 text-5xl text-center">
+                    LOKET ANTRIAN FARMASI
                 </p>
             </div>
-            <div class="p-1 border-2 border-green-600 border-gray-400 flex flex-col md:flex-row gap-1">
+            <div class="p-1 border-2 xl:border-4 border-blue-500 flex flex-col md:flex-row gap-1">
                 <div class="w-full md:w-[45%]">
-                    <iframe class="w-full h-[345px]" src="https://www.youtube.com/embed/rTu824dilZo"
+                    <iframe class="w-full h-[500px]" src="https://www.youtube.com/embed/rTu824dilZo"
                         title="YouTube video player" frameborder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                         allowfullscreen></iframe>
-                    <div class="w-full border-2 border-green-600 mt-1.5">
+                    <div class="w-full border-2 border-blue-500 mt-1.5">
                         <div class="flex justify-center items-center p-2">
                             <div class="w-full text-center">
-                                <p class="text-xl text-green-700 font-bold underline"><span id="nm_pasien"></span></p>
-                                <p class="text-base text-green-700 leading-5 font-mono">
+                                <p class="text-2xl font-bold underline"><span id="nm_pasien"></span></p>
+                                <p class="text-xl leading-5 font-mono">
                                     No Resep : <span id="no_resep">-</span>
                                 </p>
                             </div>
@@ -38,47 +38,47 @@
 
                         <div class="grid grid-cols-2">
                             <div
-                                class="flex justify-between items-center my-1 p-1.5 px-2 border-r-2 border-y-2 border-green-600">
+                                class="flex justify-between items-center my-1 p-1.5 px-2 border-r-2 border-y-2 border-blue-500">
                                 <div class="w-full">
-                                    <p class="text-sm font-bold text-green-700 leading-5 font-mono">Non Racikan</p>
+                                    <p class="text-sm md:text-base xl:text-xl font-bold leading-5 font-mono">Non Racikan</p>
                                 </div>
                                 <div class="w-[30%]">
-                                    <p class="text-sm text-green-700 leading-5 font-mono text-right" id="cnon_racik">0</p>
+                                    <p class="text-sm md:text-base xl:text-xl leading-5 font-mono text-right" id="cnon_racik">0</p>
                                 </div>
                             </div>
 
                             <div
-                                class="flex justify-between items-center my-1 p-1 px-2 border-x-0 border-y-2 border-green-600">
+                                class="flex justify-between items-center my-1 p-1 px-2 border-x-0 border-y-2 border-blue-500">
                                 <div class="w-full">
-                                    <p class="text-sm font-bold text-green-700 leading-5 font-mono">Racikan</p>
+                                    <p class="text-sm md:text-base xl:text-xl font-bold leading-5 font-mono">Racikan</p>
                                 </div>
                                 <div class="w-[30%]">
-                                    <p class="text-sm text-green-700 leading-5 font-mono text-right" id="cracik">0</p>
+                                    <p class="text-sm md:text-base xl:text-xl leading-5 font-mono text-right" id="cracik">0</p>
                                 </div>
                             </div>
                         </div>
 
                         <div class="flex justify-between items-center my-1 px-2">
                             <div class="w-full">
-                                <p class="text-sm font-bold text-green-700 leading-5 font-mono">Selesai</p>
+                                <p class="text-sm md:text-base xl:text-xl font-bold leading-5 font-mono">Selesai</p>
                             </div>
                             <div class="w-[30%]">
-                                <p class="text-sm text-green-700 leading-5 font-mono text-right" id="cselesai">0</p>
+                                <p class="text-sm md:text-base xl:text-xl leading-5 font-mono text-right" id="cselesai">0</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="w-full bg-green-200">
-                    <div class="h-[480px] overflow-y-scroll">
+                <div class="w-full">
+                    <div class="h-[660px] overflow-y-scroll">
                         <table class="table-auto w-full">
-                            <thead class="sticky top-0 bg-green-300">
-                                <tr class="border-2 border-green-100">
-                                    <th class="p-3 px-5 border-2 border-green-100 text-left text-xl text-green-600">No Resep</th>
-                                    <th class="p-3 px-5 border-2 border-green-100 text-left text-xl text-green-600">Nama</th>
-                                    <th class="p-3 px-5 border-2 border-green-100 text-left text-xl text-green-600">Jam Resep</th>
-                                    <th class="p-3 px-5 border-2 border-green-100 text-left text-xl text-green-600">Kategori</th>
-                                    <th class="p-3 px-5 border-2 border-green-100 text-left text-xl text-green-600">Status</th>
-                                    <th class="p-3 px-5 border-2 border-green-100 text-left text-xl text-green-600">Jam Selesai</th>
+                            <thead class="sticky top-0 bg-blue-100">
+                                <tr class="border-2 border-blue-300">
+                                    <th class="p-3 px-5 border-2 border-blue-300 text-center text-2xl text-blue-600">No Resep</th>
+                                    <th class="p-3 px-5 border-2 border-blue-300 text-center text-2xl text-blue-600">Nama</th>
+                                    <th class="p-3 px-5 border-2 border-blue-300 text-center text-2xl text-blue-600">Jam Resep</th>
+                                    <th class="p-3 px-5 border-2 border-blue-300 text-center text-2xl text-blue-600">Kategori</th>
+                                    <th class="p-3 px-5 border-2 border-blue-300 text-center text-2xl text-blue-600">Status</th>
+                                    <th class="p-3 px-5 border-2 border-blue-300 text-center text-2xl text-blue-600">Jam Selesai</th>
                                 </tr>
                             </thead>
                             <tbody></tbody>
@@ -156,17 +156,17 @@
                     $.each(data, function(key, value) {
 
                         if (value.no_resep == localStorage.getItem('no_panggil')) {
-                            html += "<tr id='" + value.no_resep + "' class='bg-yellow-300'>";
+                            html += "<tr id='" + value.no_resep + "' class='bg-yellow-200 font-bold'>";
                         } else {
-                            html += "<tr id='" + value.no_resep + "' class=''>";
+                            html += "<tr id='" + value.no_resep + "' class='text-blue-700 font-medium'>";
                         }
 
-                        html += "<td class='p-1 px-5 border-2 border-green-100 text-left text-base text-green-600'>" + value.no_resep + "</td>";
-                        html += "<td class='p-1 px-5 border-2 border-green-100 text-left text-base text-green-600'>" + value.nm_pasien + "</td>";
-                        html += "<td class='p-1 px-5 border-2 border-green-100 text-left text-base text-green-600'>" + value.jam_peresepan + "</td>";
-                        html += "<td class='p-1 px-5 border-2 border-green-100 text-left text-base text-green-600'>" + value.category_obat + "</td>";
-                        html += "<td class='p-1 px-5 border-2 border-green-100 text-left text-base text-green-600'>" + value.status_obat + "</td>";
-                        html += "<td class='p-1 px-5 border-2 border-green-100 text-left text-base text-green-600'>" + value.jam_penyerahan + "</td>";
+                        html += "<td class='py-3 px-5 border-2 border-blue-300 text-center text-base xl:text-xl'>" + value.no_resep + "</td>";
+                        html += "<td class='py-3 px-5 border-2 border-blue-300 text-left text-base xl:text-xl'>" + value.nm_pasien + "</td>";
+                        html += "<td class='py-3 px-5 border-2 border-blue-300 text-center text-base xl:text-xl'>" + value.jam_peresepan + "</td>";
+                        html += "<td class='py-3 px-5 border-2 border-blue-300 text-center text-base xl:text-xl'>" + value.category_obat + "</td>";
+                        html += "<td class='py-3 px-5 border-2 border-blue-300 text-center text-base xl:text-xl'>" + value.status_obat + "</td>";
+                        html += "<td class='py-3 px-5 border-2 border-blue-300 text-center text-base xl:text-xl'>" + value.jam_penyerahan + "</td>";
                         html += "</tr>";
 
                         if (value.status_obat == "SELESAI") {
