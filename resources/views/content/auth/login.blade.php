da6dd6938947b4e553aba04ddd732f377c92245d
muchron <msfiqih@gmail.com>
2023-09-06 10:40:24 +0700

    clear history pencarian kamar

--- resources/views/content/auth/login.blade.php
+++ resources/views/content/auth/login.blade.php
@@ -102,6 +102,7 @@
 <script type="text/javascript">
     document.addEventListener("DOMContentLoaded", () => {
         localStorage.removeItem('pasien')
+        localStorage.removeItem('kamar')
         localStorage.removeItem('nm_pasien')
         localStorage.removeItem('tanggal')
         localStorage.removeItem('tgl_awal')
