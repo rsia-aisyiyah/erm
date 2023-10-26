2dd29af82f0c0ccbd5a86fda4af54f16a6ea2ba8
muchron <msfiqih@gmail.com>
2023-10-26 10:53:22 +0700

    app config sim

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://sim.rsiaaisyiyah.com/erm'),
 
     'asset_url' => env('ASSET_URL', null),
 
