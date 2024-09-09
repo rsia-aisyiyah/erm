3addbfa9d0a1c175350512c0d06da3d7894d3ff5
msfiqih <msfiqih@gmail.com>
2024-09-09 09:26:29 +0700

    Fix create resume medis ranap

--- app/Http/Controllers/ResumePasienRanapController.php
+++ app/Http/Controllers/ResumePasienRanapController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\ResumePasienRanap;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 
 class ResumePasienRanapController extends Controller
@@ -16,7 +17,7 @@ class ResumePasienRanapController extends Controller
         $this->track = new TrackerSqlController();
     }
 
-    function get(Request $request)
+    public function get(Request $request)
     {
         $resume = $this->resume->where('no_rawat', $request->no_rawat)->with([
             'regPeriksa.pasien', 'dokter.spesialis', 'regPeriksa.penjab', 'regPeriksa.poliklinik',
@@ -24,23 +25,39 @@ class ResumePasienRanapController extends Controller
                 return $query->where('stts_pulang', '!=', 'Pindah Kamar')->with(['kamar.bangsal']);
             }, 'bayiGabung.kamarIbu' => function ($query) {
                 return $query->where('stts_pulang', '!=', 'Pindah Kamar')->with(['kamar.bangsal']);
-            }
+            },
         ])->first();
 
         return response()->json($resume);
     }
 
-    function insert(Request $request)
+    public function insert(Request $request)
     {
-        $data =  $request->except(['tgl_kontrol', 'jam_kontrol', '_token']);
-        $this->track->insertSql($this->resume, $data);
-        return $this->resume->create($data);
+        try {
+            $data = $request->except(['tgl_kontrol', 'jam_kontrol', '_token']);
+            $resume = $this->resume->create($data);
+            if ($resume) {
+                $this->track->insertSql($this->resume, $data);
+
+            }
+        } catch (QueryException $e) {
+            return response()->json($e->errorInfo, 500);
+        }
+        return response()->json('Sukses');
     }
 
-    function edit(Request $request)
+    public function edit(Request $request)
     {
-        $data =  $request->except(['tgl_kontrol', 'jam_kontrol', '_token']);
-        $this->track->updateSql($this->resume, $data, ['no_rawat', $data['no_rawat']]);
-        return $this->resume->where('no_rawat', $data['no_rawat'])->update($data);
+        $data = $request->except(['tgl_kontrol', 'jam_kontrol', '_token']);
+        try {
+            $update = $this->resume->where('no_rawat', $data['no_rawat'])->update($data);
+            if ($update) {
+                $this->track->updateSql($this->resume, $data, ['no_rawat', $data['no_rawat']]);
+            }
+        } catch (QueryException $e) {
+            return response()->json($e->errorInfo, 500);
+        }
+
+        return response()->json('Sukses');
     }
 }
