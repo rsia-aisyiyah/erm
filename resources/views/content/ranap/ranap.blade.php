5ab01e7f985fffdca48b788dc4e41bf132f22e53
msfiqih <msfiqih@gmail.com>
2025-12-15 11:32:57 +0700

    label modal tambah nilai ews

--- resources/views/content/ranap/ranap.blade.php
+++ resources/views/content/ranap/ranap.blade.php
@@ -172,7 +172,7 @@
         <div class="modal-dialog modal-lg modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="modalEwsRanapLabel">Modal title</h1>
+                    <h1 class="modal-title fs-5" id="modalEwsRanapLabel">Tambah Nilai EWS</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
