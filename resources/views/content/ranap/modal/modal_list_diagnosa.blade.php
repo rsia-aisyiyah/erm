69aa3d491914cc40956679361d701a02e4f38299
msfiqih <msfiqih@gmail.com>
2026-01-26 08:54:47 +0700

    diagnosa resume medis from penilaian/asesmen soap

--- resources/views/content/ranap/modal/modal_list_diagnosa.blade.php
+++ resources/views/content/ranap/modal/modal_list_diagnosa.blade.php
@@ -6,19 +6,44 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="dxpx-tab" data-bs-toggle="tab" data-bs-target="#dxpx-tab-pane" type="button" role="tab" aria-controls="dxpx-tab-pane" aria-selected="true">Home</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="pemeriksaan-tab" data-bs-toggle="tab" data-bs-target="#pemeriksaan-tab-pane" type="button" role="tab" aria-controls="pemeriksaan-tab-pane" aria-selected="false">Profile</button>
+                    </li>
 
-                <table class="table table-striped" id="tbListDiagnosa">
-                    <thead>
-                        <tr>
-                            <th class="dxpx">KODE</th>
-                            <th>NAMA</th>
-                            <th>KETERANGAN</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                </ul>
+                <div class="tab-content p-2" id="dxpxTabContent">
+                    <div class="tab-pane fade show active" id="dxpx-tab-pane" role="tabpanel" aria-labelledby="dxpx-tab" tabindex="0">
+                        <table class="table table-striped table-hover" id="tbListDiagnosa">
+                            <thead>
+                                <tr>
+                                    <th class="dxpx">KODE</th>
+                                    <th>NAMA</th>
+                                    <th>KETERANGAN</th>
+                                </tr>
+                            </thead>
+                            <tbody>
 
-                    </tbody>
-                </table>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="tab-pane fade" id="pemeriksaan-tab-pane" role="tabpanel" aria-labelledby="pemeriksaan-tab" tabindex="0">
+                        <table class="table table-striped table-hover" id="tbPemeriksaanSoap">
+                            <thead>
+                                <tr>
+                                    <th>Tgl. Periksa</th>
+                                    <th>Hasil Pemeriksaan</th>
+                                    <th>Dokter</th>
+                                </tr>
+                            </thead>
+                            <tbody></tbody>
+                        </table>
+                    </div>
+                </div>
+
             </div>
             <div class="modal-footer" style="justify-content:flex-start">
                 <div class="col-sm-12 col-md-6 col-lg-6">
@@ -48,7 +73,6 @@
                     getDiagnosa(keyword).done((response) => {
                         let no = 1;
                         $.map(response, (dx) => {
-                            console.log(dx);
                             row = `<tr class="${no}" onclick="setTextRiwayat('${parameter}', ${no} )" style="cursor:pointer">`
                             row += `<td>${dx.kd_penyakit}</td>`
                             row += `<td>${dx.nm_penyakit}</td>`
@@ -60,7 +84,6 @@
                     });
                     break;
                 case 'prosedur':
-                    console.log('xxxx');
                     getProsedur(keyword).done((response) => {
                         let no = 1;
                         $.map(response, (px) => {
@@ -93,6 +116,40 @@
             $('#modalListDiagnosa input[name=kode_diagnosa]').val('')
             $('#modalListDiagnosa input[name=nama_diagnosa]').val('')
             $('#modalListDiagnosa input[name=dxpx]').val('')
+            $('button[data-bs-target="#dxpx-tab-pane"]').trigger('click');  
         })
+
+        $('button[data-bs-target="#pemeriksaan-tab-pane"]').on('shown.bs.tab', function(e, x, y) {
+            const no_rawat = $('#modalResumeRanap').find('input[name=no_rawat]').val()
+            $('#tbPemeriksaanSoap tbody').empty();
+            getPemeriksaanRanap(no_rawat).done((response) => {
+                const pemeriksaan = response.filter(item => item.pegawai.dokter !== null).map((item, index) => {
+                    return `` +
+                        `<tr onclick="setDiagnosaResumeFromSoap('${item.penilaian}')">` +
+                        `<td>${item.tgl_perawatan}</td>` +
+                        `<td>${item.penilaian}</td>` +
+                        `<td>${item.pegawai.dokter.nm_dokter}</td>` +
+                        `</tr>`;
+                })
+                $('#tbPemeriksaanSoap tbody').append(pemeriksaan.join(''))
+                $('#tbPemeriksaanSoap tbody tr').each(function(i) {
+                    $(this).addClass(i).css('cursor', 'pointer');
+                })
+
+            })
+
+        })
+
+        function setDiagnosaResumeFromSoap(penilaian) {
+
+            const namaDxTarget = $('#modalListDiagnosa input[name=nama_diagnosa]').val();
+            const kdDxTarget = $('#modalListDiagnosa input[name=kode_diagnosa]').val();
+
+            $(`#modalResumeRanap input[name=${namaDxTarget}]`).val(penilaian);
+            $(`#modalResumeRanap input[name=${kdDxTarget}]`).val('-');
+
+            $('#modalListDiagnosa').modal('hide');
+
+        }
     </script>
 @endpush
